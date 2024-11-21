a5e1c2a8d5a673708cb76fa7c73af5306c1eaeee
nickyin983 <nickyin@tw.ibm.com>
2024-11-21 22:16:57 +0800

    u

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://taiwanhelper.com/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
